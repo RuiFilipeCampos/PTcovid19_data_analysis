8f407ae698e8ef09c72a4d406803718b5c0105ed
unknown <63464503+RuiFilipeCampos@users.noreply.github.com>
2021-09-19 15:39:42 +0100

    MINOR:setup

--- _globals.py
+++ _globals.py
@@ -7,5 +7,19 @@ import matplotlib as plt
 from pathlib import Path
 
 
+__files__ = [
+    "data.csv",
+    "data_concelhos.csv",
+    "data_concelhos_14dias.csv",
+    "data_concelhos_incidencia.csv",
+    "data_concelhos_new.csv",
+    "rt.csv",
+    "vacinas.csv",
+    "vacinas_detalhe.csv",
+]
+
 class PATH:
-  ROOT = Path(".")
+    ROOT = Path(".")
+    DATA = ROOT/'covid19pt-data'
+    data = DATA/"data.csv"
+    
