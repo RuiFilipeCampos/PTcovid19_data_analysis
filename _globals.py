1725af06e012b0e8e701e7e5704957ccb5e175f3
Rui Campos <ruifilipedesousacampos@gmail.com>
2021-09-19 15:34:05 +0100

    Create _globals.py

---
+++ _globals.py
@@ -0,0 +1,11 @@
+"""Vars and modules shared by all notebooks.
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from pathlib import Path
+
+
+class PATH:
+  ROOT = Path(".")
